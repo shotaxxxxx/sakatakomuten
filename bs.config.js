2035119526e54edeb7383f267f2b357bea47c4af
hilosiva <sbthrnr@icloud.com>
2020-03-17 00:05:51 +0900

    Revert "proxyno"
    This reverts commit f2a84fb2a2da38f5a895c68b8b2ec874c15cb01d.

--- bs.config.js
+++ bs.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   "files": './public/**/*.css, ./public/**/*.js, ./public/**/*.html, ./public/**/*.php',
-  "server": { // PHPを使う時は server オブジェクトをコメントにする
+  "server": {
     baseDir: './public/',
     index: 'index.html'
   },
-  // "proxy": 'http://localhost:8080/',  // PHPを使う時用
   "online": true,
   "open": 'external',
   "proxy": false,
