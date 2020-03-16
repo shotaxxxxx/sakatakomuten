94f1df526e90f1aa6160c321d03fb41adcb26045
hilosiva <sbthrnr@icloud.com>
2020-03-17 00:06:59 +0900

    proxyの設定をコメントで追加

--- bs.config.js
+++ bs.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   "files": './public/**/*.css, ./public/**/*.js, ./public/**/*.html, ./public/**/*.php',
-  "server": {
+  "server": { // PHPを使う時は server オブジェクトをコメントにする
     baseDir: './public/',
     index: 'index.html'
   },
+  // "proxy": 'http://localhost:8080/',  // PHPを使う時用
   "online": true,
   "open": 'external',
   "proxy": false,
