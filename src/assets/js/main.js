6361d81bcd10e0da84575c09bf1817509014ce01
hilosiva <sbthrnr@icloud.com>
2021-03-03 13:11:48 +0900

    バージョンアップ

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,16 @@
-import $ from 'jquery';
+// Polyfill
+import 'intersection-observer';
+import objectFitImages from 'object-fit-images';
+import closetPolyfill from './lib/closet.polyfill.js';
+objectFitImages();
+closetPolyfill();
 
-$(function() {
-  console.log('Hello jQuery');
-});
+// Libraly
+// import $ from 'jquery';
+// import Swiper from 'swiper';
+// import ScrollObserver from './lib/ScrollObserver.js';
+// import Toggle from './lib/Toggle.js';
+// import { dropdown } from './lib/dropdown.js';
+// import { inview } from './lib/inview.js';
+
+import './lib/loading.js';
