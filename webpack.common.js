0915d054daa5a05c465d29c9bc970e0d3ef64030
hilosiva <sbthrnr@icloud.com>
2021-04-01 13:30:26 +0900

    use -> loader に変更
    webpack５の仕様に伴い、useをloaderに変更

--- webpack.common.js
+++ webpack.common.js
@@ -92,7 +92,7 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: "ts-loader",
+        loader: "ts-loader",
       },
       {
         test: /\.js$/,
