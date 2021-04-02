ee47e5e1e80ae0a0480725e6b6768e9b128acc56
hilosiva <sbthrnr@icloud.com>
2021-04-02 17:15:00 +0900

    meta要素の変更

--- src/php/index.php
+++ src/php/index.php
@@ -2,6 +2,15 @@
   require_once('include/config.php');
   require_once('../config/php/database.php');
 
+  // ページの設定
+  $page = [
+    'title'       => SITE_NAME . ' | 〇〇〇〇',
+    'description' => '各ページ固有の説明文を124文字程度で',
+    'path'        => ROOT_PATH,
+    'image'       => SITE_URL . 'OGP画像のファイルパス'
+  ];
+
+
   try {
     $dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
     echo '接続成功';
